aaeb04ef8bffd42fdd132a13e9b07bd1f4c4724e
hashimalghamdi <hashimghamdi@outlook.com>
2025-03-08 16:17:12 +0300

    add husky

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,31 +1,25 @@
-import Link from "next/link";
-import React, { useState } from "react";
-import { Product } from "@/interfaces/Product";
-import { useImageError } from "@/hooks/useImageError";
-import ImagePlaceholderComponent from "./ImagePlaceholderComponent";
-import SaudiRiyal from "@/assets/images/saudi-riyal.svg";
-import Image from "next/image";
-import { useCategories } from "@/contexts/CategoriesContext";
-import { useCart } from "@/contexts/CartContext";
-import QuantityControls from "./QuantityControls";
-import { useRouter } from "next/navigation";
-import { useAuth } from "@/contexts/AuthContext";
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { Product } from '@/interfaces/Product';
+import { useImageError } from '@/hooks/useImageError';
+import ImagePlaceholderComponent from './ImagePlaceholderComponent';
+import SaudiRiyal from '@/assets/images/saudi-riyal.svg';
+import Image from 'next/image';
+import { useCategories } from '@/contexts/CategoriesContext';
+import { useCart } from '@/contexts/CartContext';
+import QuantityControls from './QuantityControls';
+import { useRouter } from 'next/navigation';
 
 const ProductCard = ({ product }: { product: Product }) => {
   const { handleImageError, hasImageError } = useImageError();
   const { categories } = useCategories();
-  const { addToCart, cart, updateCartItem, deleteCartItem } = useCart();
+  const { cart, updateCartItem, deleteCartItem } = useCart();
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
-  const { isLoggedIn } = useAuth();
 
-  const category = categories.find(
-    (category) => category.id === product.categoryId
-  );
+  const category = categories.find(category => category.id === product.categoryId);
 
-  const cartItem = cart?.cartItems.find(
-    (item) => item.product.id === product.id
-  );
+  const cartItem = cart?.cartItems.find(item => item.product.id === product.id);
 
   const handleUpdateQuantity = async (newQuantity: number) => {
     if (cartItem) {
@@ -56,11 +50,8 @@ const ProductCard = ({ product }: { product: Product }) => {
 
   return (
     <div className="rounded-lg border-2 border-gray-50 flex flex-col items-start justify-start md:p-3 p-2 relative">
-      <Link
-        href={`/product/${product.id}`}
-        className="block w-full relative mb-4"
-      >
-        {product.imageURL !== "" && !hasImageError(product.id) ? (
+      <Link href={`/product/${product.id}`} className="block w-full relative mb-4">
+        {product.imageURL !== '' && !hasImageError(product.id) ? (
           <img
             src={product.imageURL}
             alt={product.name}
@@ -69,10 +60,7 @@ const ProductCard = ({ product }: { product: Product }) => {
           />
         ) : (
           <div className="w-full h-48 flex items-center justify-center">
-            <ImagePlaceholderComponent
-              productTitle={product.name}
-              showTitle={false}
-            />
+            <ImagePlaceholderComponent productTitle={product.name} showTitle={false} />
           </div>
         )}
       </Link>
@@ -81,13 +69,11 @@ const ProductCard = ({ product }: { product: Product }) => {
           <a href="#" className="block w-full text-primary text-center">
             <h2 className="text-sm">{product.name}</h2>
           </a>
-          <small className="block text-xs w-full text-center">
-            {product.description}
-          </small>
+          <small className="block text-xs w-full text-center">{product.description}</small>
         </div>
         <div className="flex items-center justify-center flex-wrap gap-2 text-gray-300 w-full">
           <span
-            onClick={(e) => category && handleCategoryClick(e, category.id)}
+            onClick={e => category && handleCategoryClick(e, category.id)}
             className="text-xs text-gray-500 underline cursor-pointer hover:text-primary transition-colors"
           >
             {category?.categoryName}
@@ -96,10 +82,10 @@ const ProductCard = ({ product }: { product: Product }) => {
       </div>
       <div className="flex flex-col sm:flex-row items-center justify-center w-full my-4 gap-1">
         <span className="font-medium text-md">
-          {product.price.toLocaleString("en-US", {
+          {product.price.toLocaleString('en-US', {
             minimumFractionDigits: 2,
             maximumFractionDigits: 2,
-          })}{" "}
+          })}{' '}
         </span>
         <Image src={SaudiRiyal} alt="Saudi Riyal" width={16} height={16} />
       </div>
